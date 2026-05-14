40ef369b639cd2b40384a58549057e76febcf325
mdkriza-cpu <mdkriza@gmail.com>
2026-05-14 11:46:37 -0400

    Update main.py

--- main.py
+++ main.py
@@ -42,9 +42,13 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://truenoise.org",
+        "https://www.truenoise.org",
+        "https://mdkriza-cpu.github.io",
+    ],
     allow_credentials=False,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST"],
     allow_headers=["*"],
 )
 
